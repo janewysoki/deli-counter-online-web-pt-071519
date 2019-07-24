e54289a48ecb60e610020755655072ff9c909ffd
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-24 16:28:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.count >= 1
    current_line = "The line is currently: "
-  katz_deli.each_with_index do |name, index|
-    puts current_line << "#{index+1}. #{name}"
-  end
+    katz_deli.each_with_index do |name, index|
+      current_line << "#{index+1}. #{name}"
+    end
+    puts current_line
   end
 end
 
