4ddc2e9df5aec385eee25e8c9b88969bf07a7504
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-24 15:51:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   elsif katz_deli.count >= 1
-
-    puts "The line is currently #{katz_deli}"
+    puts "The line is currently: #{katz_deli}"
   end
 end
 
