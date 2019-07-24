c5fed487cda2a1456cf985c82eee6ff57816f3dd
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-24 16:35:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
       current_line << "#{index+1}. #{name} "
     end
-    current_line.rstrip
-    puts current_line
+    puts current_line.rstrip
   end
 end
 
