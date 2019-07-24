883ec390addf43aef70ccfab590bd184166ac21c
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-24 15:50:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  elsif katz_deli.count >= 1
+
+    puts "The line is currently #{katz_deli}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  new_array = []
+  katz_deli.each_with_index do |name, index|
+    new_array << "Welcome, #{name}. You are number #{index+1} in line."
+  end
+  new_array
+end
+
+def now_serving(katz_deli)
+end
